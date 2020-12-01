ad101989586d402aecc67c9440fa8e195b04ccb0
Huy <phamlehuy116@gmail.com>
2020-12-01 10:04:48 +0700

    TestCount

---
+++ src/Components/CounterExam.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+function CounterExam(){
+    const [count,setCount] = useState(0);
+
+    return(
+        <div>
+            <h1>
+                {count}
+            </h1>
+           <button onClick={() => setCount(count+1)}>
+                +
+           </button>
+           <button onClick={() => setCount(count-1)}>
+                -
+           </button>
+        </div>
+    )
+}
+
+export default CounterExam;
